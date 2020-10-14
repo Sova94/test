3b16cdb33744d63e00040de64367c9b807bdcc0a
Sova94 <72863826+Sova94@users.noreply.github.com>
2020-10-14 13:37:56 +0300

    Some changes iii

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f86d2f541c25f001ec3dae5",
+				"5f86d2f541c25f001ec3dae7",
+				"5f86d35c8a25ab001e4e84c9"
+			]
+		},
+		"5f86d2f541c25f001ec3dae5": {
+			"id": "5f86d2f541c25f001ec3dae5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f86d2f541c25f001ec3dae7": {
+			"id": "5f86d2f541c25f001ec3dae7",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5f86d35c8a25ab001e4e84c9": {
+			"id": "5f86d35c8a25ab001e4e84c9",
+			"pageUrl": "test1",
+			"name": "test1"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
